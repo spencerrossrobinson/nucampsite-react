5ea767985783a296aa751f7ad41700243b2660cb
Spencer Robinson <spencerwarmouth@gmail.com>
2022-12-05 14:38:55 -0500

    slice

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,17 +1,14 @@
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
-const CampsiteCard = (props) => {
-    return (
-        <Card>
-            <CardImg
-                width='100%'
-                src={props.campsite.image}
-                alt={props.campsite.name}
-            />
-            <CardImgOverlay>
-                <CardTitle>{props.campsite.name}</CardTitle>
-            </CardImgOverlay>
-        </Card>
-    )
-}
+const CampsiteCard = ({ campsite }) => {
+  const { image, name } = campsite;
+  return (
+    <Card>
+      <CardImg width="100%" src={image} alt={name} />
+      <CardImgOverlay>
+        <CardTitle>{name}</CardTitle>
+      </CardImgOverlay>
+    </Card>
+  );
+};
 export default CampsiteCard;
