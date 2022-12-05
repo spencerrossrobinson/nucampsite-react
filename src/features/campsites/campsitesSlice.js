5a0e6257a7e818e080d63db99102b4858a0eed82
Spencer Robinson <spencerwarmouth@gmail.com>
2022-12-05 15:36:29 -0500

    useState

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -4,6 +4,11 @@ export const selectAllCampsites = () => {
   return CAMPSITES;
 };
 
-export const selectRandomCampsite = () => {
-  return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
+// placehold random function
+// export const selectRandomCampsite = () => {
+//   return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
+// };
+
+export const selectCampsiteById = (id) => {
+  return CAMPSITES.find((campsite) => campsite.id === id);
 };
