326352c3a7706e266d9d0387204db3967bfd7da1
Spencer Robinson <spencerwarmouth@gmail.com>
2022-12-31 12:53:41 -0500

    Week 5 Workshop

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -3,10 +3,27 @@ import Comment from "./Comment";
 import { selectCommentsByCampsiteId } from "./commentsSlice";
 import CommentForm from "./CommentForm";
 import { useSelector } from "react-redux";
+import Loading from "../../components/Loading";
+import Error from "../../components/Error";
 
 const CommentsList = ({ campsiteId }) => {
   const comments = useSelector(selectCommentsByCampsiteId(campsiteId));
-
+  const isLoading = useSelector((state) => state.comments.isLoading);
+  const errMsg = useSelector((state) => state.comments.errMsg);
+  if (isLoading) {
+    return (
+      <Col>
+        <Loading />
+      </Col>
+    );
+  }
+  if (errMsg) {
+    return (
+      <Col>
+        <Error errMsg={errMsg} />
+      </Col>
+    );
+  }
   if (comments && comments.length > 0) {
     return (
       <Col md="5" className="m-1">
